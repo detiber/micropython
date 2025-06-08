98c0a69d880da1a627377aabd6362d02716cef3d
Jason DeTiberus <detiber@users.noreply.github.com>
2025-06-08 05:30:19 +0000

    update flash devices after clarifaction

--- ports/raspberrypi/boards/jumperless_v5/mpconfigboard.mk
+++ ports/raspberrypi/boards/jumperless_v5/mpconfigboard.mk
@@ -7,9 +7,4 @@ CHIP_VARIANT = RP2350
 CHIP_PACKAGE = B
 CHIP_FAMILY = rp2
 
-# Not sure which of these two it should be... Difference
-# Differences are:
-# memory_type = 0x40 for xQ vs 0x70 for xM
-# write_status_register_split = false for xQ vs true for xM
-# 01_continuous_status_write undefined for xQ vs being set to true for xM
-EXTERNAL_FLASH_DEVICES = "W25Q128JVxQ,W25Q128JVxM"
+EXTERNAL_FLASH_DEVICES = "W25Q128JVxQ"
