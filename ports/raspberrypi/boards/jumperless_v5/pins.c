68e5c1d9d58ffc47765f28954ccb8f72e1f1789e
Jason DeTiberus <detiber@users.noreply.github.com>
2025-06-13 19:30:58 +0000

    add additional neopixel pins

--- ports/raspberrypi/boards/jumperless_v5/pins.c
+++ ports/raspberrypi/boards/jumperless_v5/pins.c
@@ -62,10 +62,8 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
 
     // Neopixels
     { MP_OBJ_NEW_QSTR(MP_QSTR_NEOPIXEL), MP_ROM_PTR(&pin_GPIO17) },
-
-    // { MP_ROM_QSTR(MP_QSTR_USB_HOST_DATA_PLUS), MP_ROM_PTR(&pin_GPIO1) },
-    // { MP_ROM_QSTR(MP_QSTR_USB_HOST_DATA_MINUS), MP_ROM_PTR(&pin_GPIO2) },
-    // { MP_ROM_QSTR(MP_QSTR_USB_HOST_5V_POWER), MP_ROM_PTR(&pin_GPIO11) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_NEOPIXEL0), MP_ROM_PTR(&pin_GPIO3) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_NEOPIXEL1), MP_ROM_PTR(&pin_GPIO2) },
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
